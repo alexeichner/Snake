5c7bf999ddd96785b5193eed4e89f7f9ce927c1d
alexeichner <97203845+alexeichner@users.noreply.github.com>
2023-12-18 10:59:23 -0500

    Initial Commit

---
+++ Snake/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Snake
+//
+//  Created by Alex Eichner on 12/18/23.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
