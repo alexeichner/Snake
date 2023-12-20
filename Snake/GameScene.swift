bf5e7089e77e0c2ebf46976d139a589f66dfecab
alexeichner <97203845+alexeichner@users.noreply.github.com>
2023-12-20 09:52:20 -0500

    Changed board and snake
    I made the board all white squares with a small black boarder. The snake is now contained in the the squares themselves.

--- Snake/GameScene.swift
+++ Snake/GameScene.swift
@@ -16,18 +16,21 @@ class GameScene: SKScene {
     
     // array of tuples where the first int is the row and the second is the column
     var snake: [(Int, Int)] = []
+    var headPosition = (1,1)
 //    var board: Board!
     
     override func didMove(to view: SKView) {
-        testNode = SKSpriteNode(color: NSColor.blue, size: CGSize(width: 20.0, height: 20.0))
-        testNode.position = CGPoint(x: 400, y: 400)
-        testNode.zPosition = 3
-        addChild(testNode)
+//        testNode = SKSpriteNode(color: NSColor.blue, size: CGSize(width: 20.0, height: 20.0))
+//        testNode.position = CGPoint(x: 400, y: 400)
+//        testNode.zPosition = 3
+//        addChild(testNode)
         
         intializeBoard(size: 15)
         squares[1][1].isSnake = true
-        squares[1][2].isSnake = true
-        squares[1][3].isSnake = true
+        squares[1][1].color = NSColor.systemGreen
+        //squares[1][2].isSnake = true
+        //squares[1][3].isSnake = true
+        headPosition = updateSnakePosition(direction: snakeDirection, headPositionX: 1, headPositionY: 1)
     }
     
     override func keyDown(with event: NSEvent) {
@@ -35,12 +38,16 @@ class GameScene: SKScene {
         switch event.keyCode {
         case 125:
             snakeDirection = "down"
+            headPosition = updateSnakePosition(direction: snakeDirection, headPositionX: headPosition.0, headPositionY: headPosition.1)
         case 124:
             snakeDirection = "right"
+            headPosition = updateSnakePosition(direction: snakeDirection, headPositionX: headPosition.0, headPositionY: headPosition.1)
         case 126:
             snakeDirection = "up"
+            headPosition = updateSnakePosition(direction: snakeDirection, headPositionX: headPosition.0, headPositionY: headPosition.1)
         case 123:
             snakeDirection = "left"
+            headPosition = updateSnakePosition(direction: snakeDirection, headPositionX: headPosition.0, headPositionY: headPosition.1)
         default:
             print("default")
         }
@@ -48,18 +55,18 @@ class GameScene: SKScene {
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
-        updateSnakePosition(direction: snakeDirection)
-        if snakeDirection == "down" {
-            testNode.position.y -= 2
-        } else if snakeDirection == "right" {
-            testNode.position.x += 2
-        } else if snakeDirection == "up" {
-            testNode.position.y += 2
-        } else if snakeDirection == "left" {
-            testNode.position.x -= 2
-        } else {
-            
-        }
+        
+//        if snakeDirection == "down" {
+//            testNode.position.y -= 2
+//        } else if snakeDirection == "right" {
+//            testNode.position.x += 2
+//        } else if snakeDirection == "up" {
+//            testNode.position.y += 2
+//        } else if snakeDirection == "left" {
+//            testNode.position.x -= 2
+//        } else {
+//            
+//        }
     }
     
     func intializeBoard(size: Int) {
@@ -68,20 +75,21 @@ class GameScene: SKScene {
         for row in 0..<size {
             var squareRow: [Square] = []
             for column in 0..<size {
-                if row % 2 == 0 {
-                    if column % 2 == 0 {
-                        squareRow.append(createSquare(color: NSColor.white, row: row, column: column))
-                        
-                    } else {
-                        squareRow.append(createSquare(color: NSColor.gray, row: row, column: column))
-                    }
-                } else {
-                    if column % 2 == 0 {
-                        squareRow.append(createSquare(color: NSColor.gray, row: row, column: column))
-                    } else {
-                        squareRow.append(createSquare(color: NSColor.white, row: row, column: column))
-                    }
-                }
+//                if row % 2 == 0 {
+//                    if column % 2 == 0 {
+//                        squareRow.append(createSquare(color: NSColor.white, row: row, column: column))
+//                        
+//                    } else {
+//                        squareRow.append(createSquare(color: NSColor.gray, row: row, column: column))
+//                    }
+//                } else {
+//                    if column % 2 == 0 {
+//                        squareRow.append(createSquare(color: NSColor.gray, row: row, column: column))
+//                    } else {
+//                        squareRow.append(createSquare(color: NSColor.white, row: row, column: column))
+//                    }
+//                }
+                squareRow.append(createSquare(color: NSColor.white, row: row, column: column))
             }
             squares.append(squareRow)
         }
@@ -89,7 +97,7 @@ class GameScene: SKScene {
     
     func createSquare(color: NSColor, row: Int, column: Int) -> Square {
         let square = Square(row: row, column: column)
-        square.position = CGPoint(x: 200 + (row*30), y: 200 + (column*30))
+        square.position = CGPoint(x: 200 + (row*31), y: 200 + (column*31))
         square.size = CGSize(width: 30, height: 30)
         square.color = color
         square.zPosition = 2
@@ -97,13 +105,41 @@ class GameScene: SKScene {
         return square
     }
     
-    func updateSnakePosition(direction: String) {
-        for row in 0..<squares.count {
-            for column in 0..<squares.count {
-                if squares[row][column].isSnake {
-                    squares[row][column].color = NSColor.systemGreen
-                }
-            }
+    func updateSnakePosition(direction: String, headPositionX: Int, headPositionY: Int) -> (Int, Int) {
+//        for row in 0..<squares.count {
+//            for column in 0..<squares.count {
+//                if squares[row][column].isSnake {
+//                    squares[row][column].color = NSColor.systemGreen
+//                }
+//            }
+//        }
+        
+        if snakeDirection == "down" {
+            squares[headPositionX][headPositionY].isSnake = false
+            squares[headPositionX][headPositionY].color = NSColor.white
+            squares[headPositionX][headPositionY - 1].isSnake = true
+            squares[headPositionX][headPositionY - 1].color = NSColor.systemGreen
+            return (headPositionX, headPositionY - 1)
+        } else if snakeDirection == "right" {
+            squares[headPositionX][headPositionY].isSnake = false
+            squares[headPositionX][headPositionY].color = NSColor.white
+            squares[headPositionX + 1][headPositionY].isSnake = true
+            squares[headPositionX + 1][headPositionY].color = NSColor.systemGreen
+            return (headPositionX + 1, headPositionY)
+        } else if snakeDirection == "up" {
+            squares[headPositionX][headPositionY].isSnake = false
+            squares[headPositionX][headPositionY].color = NSColor.white
+            squares[headPositionX][headPositionY + 1].isSnake = true
+            squares[headPositionX][headPositionY + 1].color = NSColor.systemGreen
+            return (headPositionX, headPositionY + 1)
+        } else if snakeDirection == "left" {
+            squares[headPositionX][headPositionY].isSnake = false
+            squares[headPositionX][headPositionY].color = NSColor.white
+            squares[headPositionX - 1][headPositionY].isSnake = true
+            squares[headPositionX - 1][headPositionY].color = NSColor.systemGreen
+            return (headPositionX - 1, headPositionY)
+        } else {
+            return (headPositionX, headPositionY)
         }
     }
     
